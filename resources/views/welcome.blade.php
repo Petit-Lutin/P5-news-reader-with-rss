4119dcc6d155e43f618653d9ff8e1f9fbe904d61
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-12-10 11:53:18 +0100

    correction authentification, ajout rôle admin (ou non) dans la table des utilisateurs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Agrégateur de blogs</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Agrégateur de blogs
                 </div>
 
                 <div class="links">
