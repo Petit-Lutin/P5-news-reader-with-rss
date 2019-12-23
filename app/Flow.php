dfdf7111648edc5833c081f1eb35c82bc9a8f026
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-12-23 09:16:29 +0100

    ajout table flux

---
+++ app/Flow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flow extends Model
+{
+    //    public function category()
+    //    {
+    //        return $this->belongsTo(Category::class);
+    //    }
+}
---
+++ database/migrations/2019_12_23_075504_create_flows_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flows', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('url');
+            $table->unsignedBigInteger('categorie_id');
+            $table->foreign('categorie_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flows');
+    }
+}
