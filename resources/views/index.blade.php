e133e5893b4aebe57b5e68eed4b0036c542a8bf4
Petit-Lutin <laurence.platzer@wanadoo.fr>
2020-02-25 11:59:48 +0100

    tri des news par date décroissante, affichage de toutes les news mélangées sur la page d'accueil, affichage des news par catégorie, affichage des news par flux

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,32 +10,38 @@
                 <a class="btn btn-primary" href="/flows/create">Ajouter un site</a>
                 <div class="indexContent">
                     <div class="flowsList">
-                        @foreach($categories as $category)
-                            <h2>{{$category->name}} <small><a
-                                        href="/categories/edit/{{$category->id}}">Modifier</a></small> <small><a
-                                        href="/category/delete/{{$category->id}}" class="toConfirm"
-                                        data-message="Voulez-vous vraiment supprimer cette catégorie ? Les flux à l'intérieur seront également supprimés de la base de données.">Supprimer</a>
-                                </small>
-                            </h2>
-                            <ul>
-                                @foreach($category->flowsOrderBy as $flow)
-                                    <li>{{$flow->name}} <a href="/flow/show/{{$flow->id}}">Voir</a>
-                                        <small><a href="/flows/edit/{{$flow->id}}">Modifier</a></small>
-                                        <small><a href="/flows/delete/{{$flow->id}}" class="toConfirm"
+                        {{--                        @foreach($categories as $category)--}}
+                        <ul>
+                            <li v-for="category in categories"><h2>@{{category.name}} <small>
+                                        <a href="#" @click="currentList=category.allNews">Voir</a>
+                                        <a
+                                            v-bind:href="'/categories/edit/'+category.id">Modifier</a></small> <small><a
+                                            v-bind:href="'/category/delete/'+category.id" class="toConfirm"
+                                            data-message="Voulez-vous vraiment supprimer cette catégorie ? Les flux à l'intérieur seront également supprimés de la base de données.">Supprimer</a>
+                                    </small>
+                                </h2>
+
+
+                                <ul>
+                                    {{--                                        @foreach($category->flowsOrderBy as $flow)--}}
+                                    <li v-for="flow in category.flows">
+                                        @{{flow.name}} <a href="#" @click="currentList=flow.news">Voir</a>
+                                        <small><a v-bind:href="'/flows/edit/'+flow.id">Modifier</a></small>
+                                        <small><a v-bind:href="'/flows/delete/'+flow.id" class="toConfirm"
                                                   data-message="Voulez-vous vraiment retirer ce site ?">Supprimer</a>
                                         </small>
                                     </li>
-                                @endforeach
-                            </ul>
-
-                        @endforeach
+                                    {{--                                        @endforeach--}}
+                                </ul>
+                            </li>
+                        </ul>
+                        {{--                        @endforeach--}}
                     </div>
 
                     <div id="flowsContent">Les flux doivent s'afficher ci-dessous, les news doivent être par triées par
                         date décroissante.
-                        {{--                        boucle  v-for sur allNews--}}
                         <ul>
-                            <li v-for="anew in allNews">
+                            <li v-for="anew in currentList">
                                 <a v-bind:href="anew.article_link">
                                     @{{ anew.article_title }}
                                 </a>, le @{{ anew.article_date }}
@@ -160,93 +166,119 @@
                                 //         }
                                 //     ]
                                 // }],
-
+                                currentList: [], // obj littéral qui contient un tableau flows qui contient un tableau news
                                 categories:{!!$jsonCategories!!},
                                 allNews: [],
 // , channel_title:"nomduflux", {article_title:"titre"}, //liste catégories contient liste flux contient liste articles, avec latest ? v-for dans un v-for dans un v-for
                                 error: false,
                                 show: false,
+                                loaded: false,
                             },
                             mounted() {
-                                {
-                                    let toLoad = 0; // au début de l'asynchrone
 
-                                    for (let c = 0; c < this.categories.length; c++) {
-                                        let category = this.categories[c];
-                                        // console.log(category);
-                                        // console.log(category.name);
+                                let toLoad = 0; // au début de l'asynchrone
 
-                                        for (let f = 0; f < category.flows.length; f++) {
-                                            let flow = category.flows[f];
-                                            // console.log("---", flow.name)
-                                            toLoad++; //compte les flux
+                                for (let c = 0; c < this.categories.length; c++) {
+                                    let category = this.categories[c];
+                                    this.categories[c].allNews = [];
+                                    // console.log(category);
+                                    // console.log(category.name);
 
-                                            axios.get('/getjson/' + flow.id) // Make a request for a user with a given ID
-                                                .then((response) => {
-                                                    // handle success
-                                                    flow.news = response.data;
-                                                    // console.log(response.data);
-                                                    // console.log(response.data[0].article_date);
+                                    for (let f = 0; f < category.flows.length; f++) {
+                                        let flow = category.flows[f];
+                                        // console.log("---", flow.name)
+                                        toLoad++; //compte les flux
 
-                                                    for (const article of response.data) {
-                                                        this.allNews.push(article);
-                                                        // this.pubDate = article.article_date;
-                                                        // console.log(typeof this.pubDate); //string
-                                                        // console.log(this.pubDate)  // renvoie la date de publication de l'article
+                                        axios.get('/getjson/' + flow.id) // Make a request for a user with a given ID, Axios en requête GET
+                                            .then((response) => {
+                                                // handle success
+                                                flow.news = response.data;
+                                                // console.log(response.data);
 
+                                                for (const article of response.data) {
+                                                    this.allNews.push(article);
+                                                    this.categories[c].allNews.push(article);
+                                                    // this.pubDate = article.article_date;
+
+                                                    // console.log(typeof this.pubDate); //string
+                                                    // console.log(this.pubDate)  // renvoie la date de publication de l'article
+                                                }
+
+                                                // this.latest = this.allNews.splice(0, 5); // les 5 dernières news affichées à part
+                                            })
+                                            .catch(function (error) {
+                                                // handle error
+                                                console.log("flow id", flow.id);
+                                                console.log(error);
+
+                                            })
+                                            .finally(() => {
+                                                // always executed
+
+                                                toLoad--; //flux chargé
+                                                if (toLoad == 0) { //quand tous les flux sont chargés, on peut trier les articles
+                                                    console.log("triez maintenant");
+                                                    // console.log(this.allNews); //undefined, mais résolu avec fonction anonyme fléchée = on ne perd plus le this
+                                                    // console.log(flow.news); // retourne bien les news, array
+                                                    // array.sort sur allNews par date antichrono
+                                                    // console.log(article); // retourne bien les news, obj
+                                                    // console.log(article.article_date); // retourne bien les dates des news
+                                                    //todo: faire un loader qui indique que la page a fini de charger, div en pos absolute, top O left 0 width 100% height 100% avec zindex important, avec bootstrap attribuer class d-none
+                                                    //this.loaded=true, faire un v-bind avec condition
+
+                                                    this.allNews = this.allNews.sort((a, b) => new Date(b.article_date) - new Date(a.article_date));
+
+                                                    for (i = 0; i < this.allNews.length; i++) {
+                                                        console.log(this.allNews[i].article_date);
+                                                    }
+                                                    for (i = 0; i < this.categories.length; i++) {
+                                                        this.categories[i].allNews = this.categories[i].allNews.sort((a, b) => new Date(b.article_date) - new Date(a.article_date));
                                                     }
 
+                                                    this.currentList = this.allNews;
 
-                                                    // this.latest = this.allNews.splice(0, 5); // les 5 dernières news affichées à part
-
-                                                })
-                                                .catch(function (error) {
-                                                    // handle error
-                                                    console.log("flow id", flow.id);
-                                                    console.log(error);
-
-                                                })
-                                                .finally(function () {
-                                                    // always executed
-
-                                                    toLoad--; //flux chargé
-
-                                                    if (toLoad == 0) { //quand tous les flux sont chargés, on peut trier les articles
-                                                        console.log("triez maintenant")
-
-                                                        // array.sort sur allNews par date antichrono
+                                                    // this.currentList = {flows: [{news: this.allNews}]}; //forme d'une catégorie
+                                                    // this.currentList = {flows: [{news: flow.news}]}; //forme d'une catégorie
 
 
-                                                        this.allNews.sort(function (a, b) {
-                                                            // Turn your strings into dates, and then subtract them
-                                                            // to get a value that is either negative, positive, or zero.
-                                                            return new Date(b.pubDate) - new Date(a.pubDate);
-                                                        });
+                                                    //
+                                                    //
+                                                    //     for (i = 0; i < article.length; i++) {
+                                                    //         let pubDates = article.article_date;
+                                                    //         let pubDate = pubDates[i];
+                                                    //         // let a = pubDate[i];
+                                                    //         // let b = pubDate[i + 1];
+                                                    //         // console.log(a);
+                                                    //         this.allSortedNews = [];
+                                                    //         // flow.news.sort(function (pubDate, pubDates[i + 1])
+                                                    //         // {
+                                                    //         //     return new Date(pubDate.date) - new Date(pubDates[i + 1].date);
+                                                    //         // }
+                                                    //     // )
+                                                    //     //     ;
+                                                    //         this.allSortedNews.push(articles);
+                                                    //     }
 
 
-                                                    }
-                                                });
-                                        }
+                                                    // array.sort(function(a,b){
+                                                    //     // Turn your strings into dates, and then subtract them
+                                                    //     // to get a value that is either negative, positive, or zero.
+                                                    //     return new Date(b.date) - new Date(a.date);
+                                                    // });
+
+
+                                                    // let allNews = [];
+
+                                                    // console.log(pubDate);
+                                                }
+
+                                            });
                                     }
+
                                 }
                             }
                         })
                     ;
-
-
-                    //     this.article = this.articles[i];
-
-                    //         // console.log(this.articles[i]); //affiche les infos de chaque article
-                    //         // console.log(response.data[0].news[i].article_date); //affiche la date de l'article
-                    //
-                    //     console.log(response.data[0].news[0]);
-                    //     // console.log(response.data[0].news[0].article_date);
-                    //     // console.log(response.data[0].news[1].article_date);
-
-                    // this.oldNews =this.news;
-                    // this.fullNews=this.news.push(this.oldNews);
-                    // this.total =this.news.push(response.data[0].news);
-
 
                     const mesBalises = document.querySelectorAll(".toConfirm");
                     // todo : une modal -> suppresion catégorie
