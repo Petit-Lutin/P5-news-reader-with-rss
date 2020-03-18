4189e8cd11a9f5ca2cc84cb20887a49f85aa012e
Petit-Lutin <laurence.platzer@wanadoo.fr>
2020-03-18 16:48:06 +0100

    nettoyage vues

--- resources/views/flows/script.blade.php
+++ resources/views/flows/script.blade.php
@@ -2,7 +2,6 @@
     function displayNewCategory() {
         var isSelected = document.getElementById("mySelect").selectedIndex;
         var valueOption = document.getElementsByTagName("option")[isSelected].value;
-        // console.log(document.getElementsByTagName("option")[isSelected].value);
         if (valueOption == "-1") {// si "Nouvelle catégorie" est sélectionné dans la liste déroulante
             document.getElementById("newCategory").style.display = "block"; //on affiche une zone de texte pour saisir cette nouvelle catégorie
             document.getElementById("newCategoryLabel").style.display = "block"; //label pour accessibilité
